4e15f49905e8b02cb381c1e8417b91d886e90f86
fabstei <fabstei@users.noreply.github.com>
2023-02-08 15:21:28 +0100

    Hide id on Form

--- src/Controller/Admin/DocumentCrudController.php
+++ src/Controller/Admin/DocumentCrudController.php
@@ -19,7 +19,7 @@ class DocumentCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             IdField::new('uuid')->hideOnForm(),
             AssociationField::new('consultation'),
             TextField::new('title'),
