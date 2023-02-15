ae03e98a349dc9dafdc748f7047db12cdc401f16
fabstei <fabstei@users.noreply.github.com>
2023-02-15 13:27:19 +0100

    Footer Newsletter

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -12,24 +12,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class NewsletterController extends AbstractController
 {
-    #[Route('/newsletter', name: 'newsletter', methods: ['GET', 'POST'])]
+    #[Route('/newsletter/subscribe', name: 'app_newsletter_subscribe', methods: ['GET', 'POST'])]
     public function subscribe(Request $request, NewsletterRepository $newsletterRepository): Response
     {
         $newsletter = new Newsletter();
-        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form = $this->createForm(NewsletterType::class, $newsletter, [
+            'action' => $this->generateUrl('app_newsletter_subscribe'),
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $newsletterRepository->add($newsletter, true);
 
-            $this->addFlash(
-                'success-newsletter',
-                'Erfolgreich angemeldet'
-            );
+            return $this->redirectToRoute('app_newsletter_success');
         }
 
         return $this->render('newsletter/subscribe.html.twig', [
             'form' => $form,
         ]);
     }
+
+    #[Route('/newsletter', name: 'app_newsletter_success', methods: ['GET'])]
+    public function success(): Response
+    {
+        return $this->render('newsletter/success.html.twig');
+    }
+
 }
