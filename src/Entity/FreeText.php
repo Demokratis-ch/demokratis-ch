c4a280df0942e76e94ffcf32fed404faef0b1b4d
fabstei <fabstei@users.noreply.github.com>
2023-02-22 14:53:03 +0100

    Dummy freetext paragraphs

---
+++ src/Entity/FreeText.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FreeTextRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: FreeTextRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class FreeText
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'uuid')]
+    private ?Uuid $uuid = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $text = null;
+
+    #[ORM\Column]
+    #[Gedmo\Timestampable(on: 'create')]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'freeTexts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Paragraph $paragraph = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $position = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Statement $statement = null;
+
+    public function __toString(): string
+    {
+        return strval($this->getParagraph().'-'.$this->getId());
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->uuid;
+    }
+
+    #[ORM\PrePersist]
+    public function setUuid(): self
+    {
+        if ($this->getUuid() === null) {
+            $this->uuid = Uuid::v4();
+        }
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getParagraph(): ?Paragraph
+    {
+        return $this->paragraph;
+    }
+
+    public function setParagraph(?Paragraph $paragraph): self
+    {
+        $this->paragraph = $paragraph;
+
+        return $this;
+    }
+
+    public function getPosition(): ?string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getStatement(): ?Statement
+    {
+        return $this->statement;
+    }
+
+    public function setStatement(?Statement $statement): self
+    {
+        $this->statement = $statement;
+
+        return $this;
+    }
+}
