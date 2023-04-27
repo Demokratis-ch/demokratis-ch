022d6930987d46a7b06b1ccb268c209acec65aa3
Lorenz <lorenz.schaefer@easybill.de>
2023-04-27 22:42:06 +0200

    tutorials

---
+++ assets/controllers/tutorial_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    currentStep = null;
+    steps = [];
+
+    initialize() {
+        this.steps = document.getElementsByClassName('tutorialStep');
+        console.log(this.steps);
+    }
+
+    start() {
+        console.log('start');
+        this.currentStep = 0;
+        this.update();
+    }
+
+    next() {
+        this.currentStep++;
+        this.update();
+    }
+
+    prev() {
+        this.currentStep--;
+        this.update();
+    }
+
+    close() {
+        this.currentStep = null;
+        this.update();
+    }
+
+    update() {
+        for (let step of this.steps) {
+            step.classList.add('hidden');
+            step.classList.remove('block');
+        }
+
+        const step = this.steps[this.currentStep];
+        if (step === undefined) {
+            this.currentStep = null;
+        } else {
+            step.classList.remove('hidden');
+            step.classList.add('block');
+            step.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+            });
+        }
+    }
+}
