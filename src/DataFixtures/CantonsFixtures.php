3e1e1c1b33990c898a0eda4fc714b438f434f7e7
fabstei <fabstei@users.noreply.github.com>
2023-10-12 16:14:55 +0200

    Update canton fixtures

--- src/DataFixtures/CantonsFixtures.php
+++ src/DataFixtures/CantonsFixtures.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\Entity\Organisation;
+use App\Enums\Cantons;
 use App\Enums\Organisations;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
@@ -12,34 +13,6 @@ use Doctrine\Persistence\ObjectManager;
 
 class CantonsFixtures extends Fixture implements FixtureGroupInterface
 {
-    public const CANTONS = [
-        ['AG' => 'Aargau'],
-        ['AI' => 'Appenzell Innerrhoden'],
-        ['AR' => 'Appenzell Ausserrhoden'],
-        ['BE' => 'Bern'],
-        ['BL' => 'Basel-Landschaft'],
-        ['BS' => 'Basel-Stadt'],
-        ['FR' => 'Freiburg'],
-        ['GE' => 'Genf'],
-        ['GL' => 'Glarus'],
-        ['GR' => 'Graubünden'],
-        ['JU' => 'Jura'],
-        ['LU' => 'Luzern'],
-        ['NE' => 'Neuenburg'],
-        ['NW' => 'Nidwalden'],
-        ['OW' => 'Obwalden'],
-        ['SG' => 'St. Gallen'],
-        ['SH' => 'Schaffhausen'],
-        ['SO' => 'Solothurn'],
-        ['SZ' => 'Schwyz'],
-        ['TG' => 'Thurgau'],
-        ['TI' => 'Tessin'],
-        ['UR' => 'Uri'],
-        ['VD' => 'Waadt'],
-        ['VS' => 'Wallis'],
-        ['ZG' => 'Zug'],
-        ['ZH' => 'Zürich'],
-    ];
 
     public static function getGroups(): array
     {
@@ -48,19 +21,16 @@ class CantonsFixtures extends Fixture implements FixtureGroupInterface
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::CANTONS as $key => $value) {
-            foreach ($value as $slug => $name) {
-
-                $organisation[$slug] = new Organisation();
-                $organisation[$slug]->setName($name);
-                $organisation[$slug]->setSlug($slug);
-                $organisation[$slug]->setDescription('Kanton '.$name);
-                $organisation[$slug]->setUrl('https://'.strtolower($slug).'.ch');
-                $organisation[$slug]->setPublic(true);
-                $organisation[$slug]->setIsPersonalOrganisation(false);
-                $organisation[$slug]->setType(Organisations::CANTON->value);
-                $manager->persist($organisation[$slug]);
-            }
+        foreach (Cantons::cases() as $key => $canton) {
+            $organisation[$canton->value] = new Organisation();
+            $organisation[$canton->value]->setName($canton->value);
+            $organisation[$canton->value]->setSlug($canton->name);
+            $organisation[$canton->value]->setDescription('Kanton '.$canton->value);
+            $organisation[$canton->value]->setUrl('https://'.strtolower($canton->name).'.ch');
+            $organisation[$canton->value]->setPublic(true);
+            $organisation[$canton->value]->setIsPersonalOrganisation(false);
+            $organisation[$canton->value]->setType(Organisations::CANTON->value);
+            $manager->persist($organisation[$canton->value]);
         }
 
         $manager->flush();
