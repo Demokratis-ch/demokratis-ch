4c0af9b813b7ab1f4884a203cbae2520bf84c6bf
fabstei <fabstei@users.noreply.github.com>
2023-11-20 12:26:28 +0100

    Optional eventId

--- src/Service/SparqlService.php
+++ src/Service/SparqlService.php
@@ -46,7 +46,7 @@ class SparqlService
               # Language code and language
               VALUES (?languageCode ?language) { ("de" <http://publications.europa.eu/resource/authority/language/DEU>) }
               ?consultation a jolux:Consultation.
-              ?consultation jolux:eventId ?id.
+              OPTIONAL { ?consultation jolux:eventId ?eventId.}
               ?consultation jolux:eventDescription ?desc.
               OPTIONAL { ?consultation jolux:eventDescription ?desc.
                 FILTER (lang(?desc) = ?languageCode)
